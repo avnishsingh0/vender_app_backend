648eb215204958e768e7e83e829ac4a8741d2eb6
avnish <avnishbdp@gmail.com>
2023-05-29 17:13:35 +0530

    backend

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://venderappbackend-production.up.railway.app/",
+    credentials: true,
+  })
+);
 app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
